51f74ee10eb1fd45999f276278f20de6f26dbf06
Sean Liebel <seanliebel@gmail.com>
2020-09-16 20:51:31 -0400

    First commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-firebase.initializeApp=({
+firebase.initializeApp({
     apiKey: "AIzaSyCYCw4TRnQ01SBTrpNglZv-6Ic-xa7Yg0I",
     authDomain: "clone-ec248.firebaseapp.com",
     databaseURL: "https://clone-ec248.firebaseio.com",
@@ -9,7 +9,7 @@ firebase.initializeApp=({
     messagingSenderId: "57963700455",
     appId: "1:57963700455:web:7d29b2a32ac0ee4b52c242",
     measurementId: "G-X11GWXQNXZ"
-});
+})
 
 const auth = firebase.auth();
 
